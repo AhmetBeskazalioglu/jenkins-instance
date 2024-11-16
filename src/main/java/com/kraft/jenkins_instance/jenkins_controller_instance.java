d0e2c0bce71031d40252b69406f8c29636aa41c3
Ahmet Beşkazalıoğlu <146031280+AhmetBeskazalioglu@users.noreply.github.com>
2024-11-17 00:00:10 +0300

    Update jenkins_controller_instance.java

--- src/main/java/com/kraft/jenkins_instance/jenkins_controller_instance.java
+++ src/main/java/com/kraft/jenkins_instance/jenkins_controller_instance.java
@@ -9,6 +9,6 @@ public class jenkins_controller_instance {
 
     @RequestMapping("/hello")
     public String hello() {
-        return "Hello Docker-Jenkins-K8s:v2";
+        return "Hello Docker-Jenkins-K8s:v3";
     }   
 }
